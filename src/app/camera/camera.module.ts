3bd7f236e539e8ab2507391b5a7611ed2326493c
mar.troncosoc <mar.troncosoc@duocuc.cl>
2024-11-24 22:25:22 +0000

    Second Commit

--- src/app/camera/camera.module.ts
+++ src/app/camera/camera.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { QrCodeModule } from 'ng-qrcode';
 import { IonicModule } from '@ionic/angular';
-
 import { CameraPageRoutingModule } from './camera-routing.module';
-
 import { CameraPage } from './camera.page';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CameraPageRoutingModule
+    CameraPageRoutingModule,
+    QrCodeModule
   ],
-  declarations: [CameraPage]
+  declarations: [CameraPage, BarcodeScanningModalComponent]
 })
 export class CameraPageModule {}
--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,19 +1,49 @@
 // camera.page.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AsistenciaService } from '../services/asistencia.service';
 import { AuthService } from '../services/auth.service';
 import { lastValueFrom } from 'rxjs';
+import { IonModal, ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { BarcodeScanner, LensFacing } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
   styleUrls: ['./camera.page.scss'],
 })
-export class CameraPage {
+export class CameraPage implements OnInit{
+  
   constructor(
     private asistenciaService: AsistenciaService,
-    private authService: AuthService
+    private authService: AuthService,
+    private modalController: ModalController,
+    private platform: Platform
   ) {}
+  
+  ngOnInit():void{
+    if(this.platform.is('capacitor')){
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners();
+    }
+  }
+  scanResult=''
 
-
+  async startScan (){
+    const modal = await this.modalController.create({
+      component: BarcodeScanningModalComponent,
+      cssClass: 'barcode-scanning-modal',
+      showBackdrop: false,
+      componentProps: { 
+        formats: [],
+        LensFacing: LensFacing.Back
+       }
+    });
+    await modal.present();
+    const {data}= await modal.onWillDismiss();
+    if(data){
+      this.scanResult = data?.barcode?.displayValue
+    }
+  }
 }
